51a913e1ed198d2c986af85232cd290088543ad6
Manuletti Desktop at SirBlvrd <actimel@yandex.ru>
2022-07-26 22:35:21 +0300

    install sass

--- buldyk-photo/src/App.vue
+++ buldyk-photo/src/App.vue
@@ -6,4 +6,11 @@
   </body>
 </template>
 
-<style scoped></style>
+<style lang="sass">
+  $font-stack: Helvetica, sans-serif
+$primary-color: #333
+
+body
+  font: 100% $font-stack
+  color: $primary-color
+</style>
